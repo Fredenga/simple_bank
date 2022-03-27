841c4c03a9d20b5def173f5c126cfca38789d1b7
Fredrick Denga <dengafred@gmail.com>
2022-03-27 21:18:00 +0300

    Enable github actions to access current repository

---
+++ api/account.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"net/http"
+
+	db "github.com/fredrick/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type CreateAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required",oneof=USD EUR CAD`
+}
+
+func (server *Server) createAccount(ctx *gin.Context){
+	var req CreateAccountRequest
+	if err := ctx.ShouldBindJson(&req); err != nil {
+		ctx.JSON(http.StatusOK, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner: req.Owner,
+		Currency: req.Currency,
+		Balance: 0,
+	}
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+}
